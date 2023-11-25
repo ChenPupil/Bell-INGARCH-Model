8a0d60f4b205fa4628478f84ba90cbac5d3c7d23
Shuang Chen <122669836+ShuangChen09@users.noreply.github.com>
2023-11-25 14:56:11 +0800

    Update Establish Bell-INGARCH model.R

--- Establish Bell-INGARCH model.R
+++ Establish Bell-INGARCH model.R
@@ -1,1 +1,12 @@
-##
+#################################
+ts <- function(n,d,a,b) {
+  x = rep(0,n)
+  mu = rep(0,n)
+  mu[1] = 1 # intial value
+  x[1] = rbell(1,mu[1])
+    for (t in c(2:n)) {
+      mu[t] = d+a*x[t-1]+b*mu[t-1]
+      x[t] = rbell(1,mu[t])
+    }
+    return(x)
+  }
